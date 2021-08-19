e9df9588c175e5bc7a570dd0a9120113996af546
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-19 14:20:16 +0700

    v0.0.3

---
+++ views/theme-core-ui/_partials/_head.php
@@ -0,0 +1,18 @@
+
+
+        <base href="./">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
+        <meta name="author" content="Łukasz Holeczek">
+        <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
+        <title>CoreUI Free Bootstrap Admin Template</title>
+        <!-- Icons-->
+        <link href="https://cdn.jsdelivr.net/npm/@coreui/icons@2.0.1/css/all.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/flag-icon-css@3.5.0/css/flag-icon.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/simple-line-icons@2.5.5/dist/styles/simple-line-icons.min.css" rel="stylesheet">
+        <!-- Main styles for this application-->
+        <link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/pace-js@1.2.4/pace-theme-default.min.css" rel="stylesheet">
---
+++ views/theme-core-ui/_partials/_script.php
@@ -0,0 +1,12 @@
+
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/pace-js@1.2.4/pace.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/perfect-scrollbar@1.4.0/dist/perfect-scrollbar.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@2.1.9/dist/js/coreui.min.js"></script>
+        <!-- Plugins and scripts required by this view-->
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@coreui/coreui-plugin-chartjs-custom-tooltips@1.0.0/dist/js/custom-tooltips.min.js"></script>
+        <script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-pjax@2.0.1/jquery.pjax.min.js"></script>
