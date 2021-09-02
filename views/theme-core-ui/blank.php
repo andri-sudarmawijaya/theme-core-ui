0f757b89073a1cb6183006d5b3923cae3763711a
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-09-02 20:51:01 +0700

    Create blank.php

---
+++ views/theme-core-ui/blank.php
@@ -0,0 +1,76 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?><!DOCTYPE html>
+<!--
+* CoreUI - Free Bootstrap Admin Template
+* @version v2.1.12
+* @link https://coreui.io
+* Copyright (c) 2018 creativeLabs Łukasz Holeczek
+* Licensed under MIT (https://coreui.io/license)
+-->
+
+<html lang="en">
+
+<head>
+    <?php $this->load->view('theme-core-ui/_partials/_head'); ?>
+</head>
+
+<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+    <header class="app-header navbar _header">
+        <?php $this->load->view('theme-core-ui/_partials/_header'); ?>
+    </header>
+    <div class="app-body" id="pjax-container">
+        <div class="sidebar _sidebar">
+            <?php $this->load->view('theme-core-ui/_partials/_sidebar'); ?>
+        </div>
+        <main class="main">
+            <!-- Breadcrumb-->
+            <ol class="breadcrumb">
+                <?php $this->load->view('theme-core-ui/_partials/_breadcrumb'); ?>
+            </ol>
+            <div class="container-fluid">
+                <div class="animated fadeIn">
+                    <!-- /.row-->
+                    <div class="card _content">
+                        <?php //$this->load->view('theme-core-ui/_partials/_content'); ?>
+
+
+                        <div class="card-body">
+                            content
+                        </div>
+                        <div class="card-footer">
+                            footer
+                        </div>
+
+
+
+                    </div>
+                    <!-- /.row-->
+                </div>
+            </div>
+        </main>
+        <aside class="aside-menu _aside">
+            <?php $this->load->view('theme-core-ui/_partials/_aside'); ?>
+        </aside>
+    </div>
+    <footer class="app-footer _footer">
+        <?php $this->load->view('theme-core-ui/_partials/_footer'); ?>
+    </footer>
+    <!-- CoreUI and necessary plugins-->
+    <?php $this->load->view('theme-core-ui/_partials/_script'); ?>
+
+    <!-- Page necessary plugins-->
+
+    <script type="text/javascript">
+        $(function() {
+            $(document).ajaxComplete(function() {
+                Pace.restart()
+            });
+            if ($.support.pjax) {
+                $(document).pjax('a[data-pjax]', '#pjax-container')
+            }
+        });
+    </script>
+</body>
+
+</html>
